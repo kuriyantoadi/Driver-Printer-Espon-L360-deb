eaca64fbd527d2d8dd391b0b46ebf39b52d898b8
kuriyantoadi <melatikuning212@gmail.com>
2018-09-07 08:41:31 +0700

    update 7 september 2018, update pertama

---
+++ install.sh
@@ -0,0 +1,23 @@
+berhasil='echo Driver Printer anda sudah terinstall'
+cek_bit=`getconf LONG_BIT`
+kosong="echo"
+
+if [ $cek_bit = 64 ]
+then
+  sudo dpkg -i /amd64/*.deb
+  sudo apt update
+  sudo apt install -f
+  $kosong
+  $berhasil
+
+elif [ $cek_bit = 32 ]
+then
+  sudo dpkg -i /i386/*.deb
+  sudo apt update
+  sudo apt install -f
+  $kosong
+  $berhasil
+
+else
+  echo "Driver Belum terinstall"
+fi
